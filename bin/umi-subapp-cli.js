c4f3152cb42d01bef9c71c4318f7aa50c0948be7
blueju <blueju@foxmail.com>
2020-09-20 18:26:49 +0800

    update

--- bin/umi-subapp-cli.js
+++ bin/umi-subapp-cli.js
@@ -1,21 +1,28 @@
 #!/usr/bin/env node
 // 表示该文件运行于node环境
 
-const path = require('path')
-
-// 
+const path = require("path");
+//
 const program = require("commander");
+program.usage("<command>");
+
+// 传入的命令行参数
+// 例如：vue init，则传入的命令行参数就是 ["init"]
+// const args = program.parse(program.args).args;
+// console.log(args.length);
+// if (args.length === 0) {
+//   program.help();
+//   return;
+// }
 
 program
-    // 定义一个 init 命令
-    .command("init")
-    // .option("-f, --foo", "enable some foo")
-    .description("创建一个 umi 子应用")
-    .alias('i')
-    .action(() => {
-        require(path.resolve(__dirname, '../commands/init.js'))
-    })
-
-if (program.args.length === 0) {
-    program.help()
-}
+  // 定义一个 init 命令
+  .command("init")
+  // 定义一个配置
+  .option("-f, --foo", "enable some foo")
+  // 对 init 命令的描述
+  .description("初始化创建一个 umi 子应用")
+  // 别名
+  .alias("i")
+  // 执行动作
+  .action(require(path.resolve(__dirname, "../commands/init")));
--- commands/init.js
+++ commands/init.js
@@ -1,37 +1,83 @@
-const program = require('commander')
+const fs = require("fs");
 
-const option = program.parse(program.args).args[0]
-const defaultName = typeof option === 'string' ? option : 'umi3-subapp'
+const ora = require("ora");
+const chalk = require("chalk");
+const program = require("commander");
+const { prompt } = require("inquirer");
+const download = require("download-git-repo");
+const { log } = require("console");
 
-// 定义提问，提问顺序从 0 开始
+const args = program.parse(program.args).args;
+
+// 定义提问集，提问顺序从 0 开始
 const questions = [
-    {
-        name: 'name',
-        // 问题
-        message: 'Project name',
-        // 回答方式
-        type: 'input',
-        default: () => {
-            return typeof option === 'string' ? option : 'umi3-subapp'
-        },
-        // 校验
-        // validate(val) {
-        //     const validate = (val.trim().split(" ")).length === 1
-        //     return validate || 'Project name is not allowed to have spaces ';
-        // },
-        // 转换
-        // transformer(val) {
-        //     return val;
-        // }
+  {
+    name: "name",
+    // 问题
+    message: "Project name",
+    // 回答方式
+    type: "input",
+    default: () => {
+      return typeof args[1] === "string" ? args[1] : "umi3-subapp";
+    },
+    // 校验
+    // validate(val) {
+    //     const validate = (val.trim().split(" ")).length === 1
+    //     return validate || 'Project name is not allowed to have spaces ';
+    // },
+    // 转换
+    // transformer(val) {
+    //     return val;
+    // }
+  },
+];
+
+module.exports = prompt(questions).then((answers) => {
+  const { name } = answers;
+
+  const projectName = name;
+  const gitUrl = "blueju/umi3-subapp-template";
+
+  const spinner = ora("Downloading please wait...");
+  spinner.start();
+
+  // 从 github 仓库拉下 template，并存储至当前目录下名称为 projectName 的文件夹内
+  download(`${gitUrl}`, `./${projectName}`, (err) => {
+    if (err) {
+      console.log(chalk.red(err));
+      process.exit();
     }
-]
+    // 更新 package.json
+    fs.readFile(`./${projectName}/package.json`, "utf8", (err, data) => {
+      if (err) {
+        spinner.stop();
+        console.error(err);
+        return;
+      }
+      const packageJson = JSON.parse(data);
+      packageJson.name = name;
+      var updatePackageJson = JSON.stringify(packageJson, null, 2);
+      fs.writeFile(
+        `./${projectName}/package.json`,
+        updatePackageJson,
+        "utf8",
+        (err) => {
+          if (err) {
+            spinner.stop();
+            console.error(err);
+            return;
+          } else {
+            spinner.stop();
+            console.log(`
+${chalk.green("初始化创建项目成功！")}
 
-module.exports =
-    prompt(questions)
-        .then(answers => {
-            const {name} = answers
-
-            const projectName = name
-            const gitUrl = name
-            const projectName = name
-        })
+${chalk.yellow(`cd ${name}`)}
+${chalk.yellow("npm install")}
+${chalk.yellow("npm start")}
+            `);
+          }
+        }
+      );
+    });
+  });
+});
